7df532ffb86e1145af09910b0b52300aac87c9b5
Juan Cruz Maciel Henning <juan_cm94@hotmail.com>
2021-04-12 00:51:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Cloud Firestore.
+const admin = require("firebase-admin");
+
+const app = admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
+
+exports.notifyNewMessage = functions.firestore
+    .document("messages/{newMessageDocId}")
+    .onCreate((snap, context) => {
+      const message = snap.data();
+      const topic = "/topics/" + message.addressee;
+      const payload = {
+        data: {
+          messageId: context.params.newMessageDocId,
+          sender: message.sender,
+          body: message.body,
+          timerInMillis: message.timerInMillis,
+        },
+      };
+      const response = app.messaging().sendToTopic(topic, payload);
+      return response;
+    });
